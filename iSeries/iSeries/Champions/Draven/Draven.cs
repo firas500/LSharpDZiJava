abf4ed6d4edb5dd83504e949c4cb52fbec4909d4
DZ191 <darkprince97@live.it>
2015-06-09 17:58:17 +0200

    iSeries: Starting Draven.

---
+++ iSeries/iSeries/Champions/Draven/Draven.cs
@@ -0,0 +1,159 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Draven.cs" company="LeagueSharp">
+//   Copyright (C) 2015 LeagueSharp
+//   
+//             This program is free software: you can redistribute it and/or modify
+//             it under the terms of the GNU General Public License as published by
+//             the Free Software Foundation, either version 3 of the License, or
+//             (at your option) any later version.
+//   
+//             This program is distributed in the hope that it will be useful,
+//             but WITHOUT ANY WARRANTY; without even the implied warranty of
+//             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//             GNU General Public License for more details.
+//   
+//             You should have received a copy of the GNU General Public License
+//             along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// <summary>
+//   The given champion class
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace iSeries.Champions.Draven
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using iSeries.Champions.Utilities;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    using SharpDX;
+
+    using Collision = LeagueSharp.Common.Collision;
+
+    /// <summary>
+    ///     The given champion class
+    /// </summary>
+    internal class Draven : Champion
+    {
+        #region Fields
+
+        /// <summary>
+        ///     The dictionary to call the Spell slot and the Spell Class
+        /// </summary>
+        private readonly Dictionary<SpellSlot, Spell> spells = new Dictionary<SpellSlot, Spell>
+        {
+           { SpellSlot.E, new Spell(SpellSlot.E, 1000f) }, 
+           { SpellSlot.R, new Spell(SpellSlot.R, 2000f) }
+        };
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Kalista" /> class.
+        /// </summary>
+        public Draven()
+        {
+            // Menu Generation
+            this.CreateMenu = MenuGenerator.Generate;
+
+            // Damage Indicator
+            DamageIndicator.Enabled = false;
+            DamageIndicator.Initialize();
+
+            // Spell initialization
+            this.spells[SpellSlot.E].SetSkillshot(250f, 130f, 1400f, false, SkillshotType.SkillshotLine);
+            this.spells[SpellSlot.R].SetSkillshot(400f, 160f, 2000f, false, SkillshotType.SkillshotLine);
+
+            Orbwalking.OnNonKillableMinion += minion =>
+            {
+                
+            };
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     <c>OnCombo</c> subscribed orbwalker function.
+        /// </summary>
+        public override void OnCombo()
+        {
+           
+        }
+
+        /// <summary>
+        ///     <c>OnDraw</c> subscribed event function.
+        /// </summary>
+        /// <param name="args">
+        ///     The event data
+        /// </param>
+        public override void OnDraw(EventArgs args)
+        {
+        }
+
+        /// <summary>
+        ///     <c>OnHarass</c> subscribed orb walker function.
+        /// </summary>
+        public override void OnHarass()
+        {
+            
+        }
+
+        /// <summary>
+        ///     <c>OnLaneclear</c> subscribed orb walker function.
+        /// </summary>
+        public override void OnLaneclear()
+        {
+            
+        }
+
+        /// <summary>
+        ///     <c>OnUpdate</c> subscribed event function.
+        /// </summary>
+        /// <param name="args">
+        ///     The event data
+        /// </param>
+        public override void OnUpdate(EventArgs args)
+        {
+            switch (Variables.Orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    this.OnCombo();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    this.OnHarass();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    this.OnLaneclear();
+                    break;
+            }
+
+            this.OnUpdateFunctions();
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        /// <summary>
+        ///     The Functions to always process
+        /// </summary>
+        private void OnUpdateFunctions()
+        {
+            
+        }
+
+        #endregion
+    }
+}

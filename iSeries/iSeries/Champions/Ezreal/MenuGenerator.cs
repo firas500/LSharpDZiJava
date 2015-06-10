58b903e24b47a5cc4ac75eb7a9c87442e41b4bd3
Corey James <Ijava.i@hotmail.co.uk>
2015-06-10 23:44:14 +0100

    Ezreal base

---
+++ iSeries/iSeries/Champions/Ezreal/MenuGenerator.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MenuGenerator.cs" company="LeagueSharp">
+//   Copyright (C) 2015 LeagueSharp
+//   
+//             This program is free software: you can redistribute it and/or modify
+//             it under the terms of the GNU General Public License as published by
+//             the Free Software Foundation, either version 3 of the License, or
+//             (at your option) any later version.
+//   
+//             This program is distributed in the hope that it will be useful,
+//             but WITHOUT ANY WARRANTY; without even the implied warranty of
+//             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//             GNU General Public License for more details.
+//   
+//             You should have received a copy of the GNU General Public License
+//             along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// <summary>
+//   TODO The menu generator.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace iSeries.Champions.Ezreal
+{
+    using LeagueSharp.Common;
+
+    /// <summary>
+    /// TODO The menu generator.
+    /// </summary>
+    internal class MenuGenerator
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     The generation function.
+        /// </summary>
+        /// <param name="root">
+        ///     The root menu
+        /// </param>
+        public static void Generate(Menu root)
+        {
+        }
+
+        #endregion
+    }
+}

a0384eeff6e0ba8c7f4de3551e50cee565d882b8
Corey James <Ijava.i@hotmail.co.uk>
2015-06-19 08:55:17 +0100

    Vayne Base

---
+++ iSeries/iSeries/Champions/Marksman/Vayne/Vayne.cs
@@ -0,0 +1,108 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Vayne.cs" company="LeagueSharp">
+//   Copyright (C) 2015 LeagueSharp
+//   
+//             This program is free software: you can redistribute it and/or modify
+//             it under the terms of the GNU General Public License as published by
+//             the Free Software Foundation, either version 3 of the License, or
+//             (at your option) any later version.
+//   
+//             This program is distributed in the hope that it will be useful,
+//             but WITHOUT ANY WARRANTY; without even the implied warranty of
+//             MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//             GNU General Public License for more details.
+//   
+//             You should have received a copy of the GNU General Public License
+//             along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// <summary>
+//   TODO The vayne.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace iSeries.Champions.Marksman.Vayne
+{
+    using System;
+
+    using iSeries.General;
+
+    /// <summary>
+    ///     The Champion Class
+    /// </summary>
+    internal class Vayne : Champion
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Vayne"/> class.
+        /// </summary>
+        public Vayne()
+        {
+            this.CreateMenu = MenuGenerator.Generate;
+
+            // TODO set spells
+
+            // TODO anti gap closer and interrupter
+        }
+
+        /// <summary>
+        ///     Gets the champion type
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="ChampionType" />.
+        /// </returns>
+        public override ChampionType GetChampionType()
+        {
+            return ChampionType.Marksman;
+            
+        }
+
+        /// <summary>
+        ///     <c>OnCombo</c> subscribed orb walker function.
+        /// </summary>
+        public override void OnCombo()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     <c>OnDraw</c> subscribed event function.
+        /// </summary>
+        /// <param name="args">
+        ///     The event data
+        /// </param>
+        public override void OnDraw(EventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     <c>OnHarass</c> subscribed orb walker function.
+        /// </summary>
+        public override void OnHarass()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     <c>OnLaneclear</c> subscribed orb walker function.
+        /// </summary>
+        public override void OnLaneclear()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     <c>OnUpdate</c> subscribed event function.
+        /// </summary>
+        /// <param name="args">
+        ///     The event data
+        /// </param>
+        public override void OnUpdate(EventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
